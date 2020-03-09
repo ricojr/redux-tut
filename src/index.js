c926195b999ffd005ec4dd14a3e60e96743567bc
ricojr <ricopehangko@gmail.com>
2020-03-09 15:30:03 +0100

    created a new redux proejct

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+//
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { createStore, applyMiddleWare } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Local imports
+
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Assets
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import './index.css';
+
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleWare(thunk)));
+
+render (
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+)
